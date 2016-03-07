4978c71a725ae617a28a964047c988de1f99092b
Longda Feng <zhongyan.feng@alibaba-inc.com>
2016-03-07 20:48:16 +0800

    refine set local mode's configuration, provide one LocalUtils.getLocalBaseConf

--- jstorm-core/src/main/java/backtype/storm/LocalCluster.java
+++ jstorm-core/src/main/java/backtype/storm/LocalCluster.java
@@ -87,11 +87,10 @@ public class LocalCluster implements ILocalCluster {
         // TODO Auto-generated method stub
         if (!Utils.isValidConf(conf))
             throw new RuntimeException("Topology conf is not json-serializable");
-        JStormUtils.setLocalMode(true);
-        conf.put(Config.STORM_CLUSTER_MODE, "local");
-        conf.put(ConfigExtension.TOPOLOGY_BACKPRESSURE_ENABLE, false);
+        
+        conf.putAll(LocalUtils.getLocalBaseConf());
         conf.putAll(Utils.readCommandLineOpts());
-
+        
         try {
             if (submitOpts == null) {
                 state.getNimbus().submitTopology(topologyName, null, Utils.to_json(conf), topology);
