4f29518878d7fb348c0bf5a02e58c8f11001ed01
硕塞 <jian.feng@alibaba-inc.com>
2017-03-01 16:10:48 +0800

    add mode folder

---
+++ jstorm-on-yarn/src/main/java/com/alibaba/jstorm/yarn/model/DSEvent.java
@@ -0,0 +1,9 @@
+package com.alibaba.jstorm.yarn.model;
+
+/**
+ * @author fengjian
+ * @since 17/2/13
+ */
+public enum DSEvent {
+    DS_APP_ATTEMPT_START, DS_APP_ATTEMPT_END, DS_CONTAINER_START, DS_CONTAINER_END
+}
